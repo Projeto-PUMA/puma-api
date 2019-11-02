48ba6c47e03e447613e040d663efe729dc5e1ebe
Icaro Oliveira <icarooliv2@gmail.com>
2019-11-02 02:21:30 -0300

    :card_file_box: adds class and skill relationship

---
+++ database/migrations/1572671992800_class_skill_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ClassSkillSchema extends Schema {
+  up () {
+    this.create('class_skills', (table) => {
+      table.increments()
+      table.integer('skill_id').unsigned().index()
+      table.foreign('skill_id').references('id').on('students').onDelete('set null')
+      table.integer('class_id').unsigned().index()
+      table.foreign('class_id').references('id').on('students').onDelete('set null')
+      table.integer('weight')
+      table.timestamps(true, true) 
+    })
+  }
+
+  down () {
+    this.drop('class_skills')
+  }
+}
+
+module.exports = ClassSkillSchema
